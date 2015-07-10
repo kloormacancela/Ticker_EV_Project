c7c924c722c8c31352f560ce7fc03b963a108898
JustinTran <justntran91@gmail.com>
2015-07-10 12:32:07 -0400

    Justin's commit feature work done on the week of 2015-07-06 to 2015-07-10

--- app/src/main/java/com/evtian/ticker/MainActivity.java
+++ app/src/main/java/com/evtian/ticker/MainActivity.java
@@ -1,14 +1,26 @@
 package com.evtian.ticker;
 
+import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.view.animation.Animation;
+import android.widget.ProgressBar;
+
+import java.util.logging.Handler;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    // class fields for the progress bar
+    private int _mProgressStatus = 0;
+    private Handler _mHandler;
 
     Button btnShowDevi, btnPair;
     @Override
@@ -24,9 +36,39 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        // Initialization of progress bar variable (sourcing from the progress bar implemented in the MainActivity.xml),
+        // ImageView (again sourcing from the image on the MainActivity.xml) and Animation is sourced from the class stored
+        // in the anim folder.
+        final ProgressBar _progressBar = (ProgressBar)findViewById(R.id.circularProgressBar);
+        final ImageView bitCoinImage = (ImageView) findViewById(R.id.imgBitcoin);
+        final Animation r = AnimationUtils.loadAnimation(this, R.anim.logorotation);
+
+        // Uses the ImageView 'bitCoinImage' property setOnClickListener and set it with a new overrided
+        // anonymous method.
+        bitCoinImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                _mProgressStatus = 0;
+                _progressBar.setProgress(0);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (_mProgressStatus < 100) {
+                            _mProgressStatus += 1;
+                            try {
+                                Thread.sleep(20);
+                                _progressBar.setProgress(_mProgressStatus);
+                            } catch (InterruptedException e) {
+                                System.out.println("got interrupt");
+                            }
+                        }
+                    }
+                }).start();
+                bitCoinImage.startAnimation(r);
+            }
+        });
         return true;
     }
-
 
     public void buttonOnClick(View v) {
 // do something when the button is clicked
